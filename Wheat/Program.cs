c78460ea4a9273d90b0d7b261d6bf308c1789a40
Manuel Reinfurt <manuel.reinfurt@me.com>
2014-10-19 11:40:04 +0200

    Disposed MonoGame

---
+++ Wheat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+using SharpDX;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+
+using Buffer = SharpDX.Direct3D11.Buffer;
+using Device = SharpDX.Direct3D11.Device;
+
+namespace Wheat
+{
+    /// <summary>
+    /// SharpDX MiniCube Direct3D 11 Sample
+    /// </summary>
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Game game = new Game();
+            game.Initialize();
+            game.Run();
+        }
+    }
+}
