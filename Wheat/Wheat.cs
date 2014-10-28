8ee55424f3f7677370be7a749458cda7fa976e2a
Manuel Reinfurt <manuel.reinfurt@me.com>
2014-10-28 21:47:00 +0100

    We have a sky!

--- Wheat/Wheat.cs
+++ Wheat/Wheat.cs
@@ -182,7 +182,7 @@ namespace Wheat
         {
             RasterizerStateDescription stateDescription = new RasterizerStateDescription
             {
-                FillMode = FillMode.Wireframe,
+                FillMode = FillMode.Solid,
                 CullMode = CullMode.None
             };
 
